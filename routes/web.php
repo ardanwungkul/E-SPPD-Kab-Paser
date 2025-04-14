8503db855817ec21a37770c989b3781ff897bfa3
ardanwungkul <ardanwungkul14@gmail.com>
2025-04-14 13:47:10 +0700

    storage link

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ use App\Http\Controllers\SubBidangController;
 use App\Http\Controllers\SubKegiatanController;
 use App\Http\Controllers\TingkatPerjalananDinasController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,6 +39,10 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
