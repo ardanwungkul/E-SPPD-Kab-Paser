644fb76f0237636c1faed814dc897e5a6ccc8b82
wildan <wildansuhendar87@gmail.com>
2025-12-04 09:38:51 +0800

    Save, Cancel size update

--- resources/views/components/button/back-button.blade.php
+++ resources/views/components/button/back-button.blade.php
@@ -1,6 +1,6 @@
 @props(['route' => null])
 <a href="{{ $route }}"
-    class=" bg-red-500 hover:bg-opacity-80 text-white rounded-lg px-3 py-2 text-xs shadow-lg flex gap-1 items-center justify-center duration-300 whitespace-nowrap w-min font-medium">
+    class=" bg-red-500 hover:bg-opacity-80 text-white rounded-lg w-24 py-2 text-xs shadow-lg flex gap-1 items-center justify-center duration-300 whitespace-nowrap font-medium">
     <svg class="w-3 h-3 stroke-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
         viewBox="0 0 24 24">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"
